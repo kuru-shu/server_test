35030a267fe025f91d230f26e2734f017d05dcc3
kuru-shu <kurushu71@gmail.com>
2024-11-24 17:41:08 +0900

    包括的エラーハンドリング(6.3)

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.get('/user/:id', (req, res) => {
   res.status(200).send(req.params.id);
 });
 
+app.get('/err', (req, res) => {
+  throw new Error('同期的なエラー');
+  console.log('errルート');
+  res.status(200).send('errルート');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send('Internal Server Error');
+});
+
 // ポート3000でサーバーを起動
 app.listen(3000, () => {
   // サーバー起動後に呼び出されるCallback
